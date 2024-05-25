234f41a124b327cb9767f934f77d3a396870a6d7
Kwabena Owusu-Darko <kobbyowusudarko@gmail.com>
2024-05-25 12:41:03 +0000

    chore: made the name bold

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>My name is Kwabena Owusu-Darko</Text>
+      <Text style={styles.text}>My name is <Text style={styles.boldName}>Kwabena Owusu-Darko</Text></Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,11 +16,15 @@ const styles = StyleSheet.create({
     backgroundColor: '#2c2c2c',
     alignItems: 'center',
     justifyContent: 'center',
-    padding: '10%'
+    padding: '10%',
+    fontWeight: '300'
   },
   text: {
     textAlign: 'center',
     fontSize: 24,
     color: '#aaaaff'
+  },
+  boldName: {
+    fontWeight: '800'
   }
 });
