7b6b99f7c39113826c45751b63a0fec7c5df47ab
Kwabena Owusu-Darko <kobbyowusudarko@gmail.com>
2024-05-25 12:31:30 +0000

    chore: changed background colour

--- App.js
+++ App.js
@@ -13,7 +13,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#2c2c2c',
     alignItems: 'center',
     justifyContent: 'center',
   },
